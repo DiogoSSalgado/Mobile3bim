e55d75957178f87634a0e3cbc984c09771d3cd3a
Diogo <diogopequeno5@gmail.com>
2022-10-25 07:50:10 -0300

    -

--- src/screens/Pedometro/index.tsx
+++ src/screens/Pedometro/index.tsx
@@ -55,7 +55,7 @@ export default function Pedometro() {
     };
 
     return (
-        <View style={styles.contaier}>
+        <View style={styles.container}>
             <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
             <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
             <Text>Walk! And watch this go up: {currentStepCount}</Text>
